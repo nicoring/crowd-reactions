10f2e99421959aec54441583ab087971b1e41ad5
Timo Djürken <timo.djuerken@gmail.com>
2016-11-19 23:21:18 +0000

    add first VideoAPI result

--- api/resources/reactions.py
+++ api/resources/reactions.py
@@ -1,8 +1,17 @@
+import json
+
 from flask_restful import Resource, Api
 
 class AdReaction(Resource):
-  def get(self):
-    return {'hello': 'world'}
+  def get(self, ad_id, reaction_id):
+    res = ''
+    with open('out.json', 'r') as f:
+      res = json.loads(f.read())
+  
+    return {
+      'id': 1,
+      'processing_result': res
+    }
 
 class AdReactionList(Resource):
   def get(self):
