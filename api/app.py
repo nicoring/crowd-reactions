cc001c50aeca666ed6433f03d12864d7f546243d
Nico Ring <ringnico@web.de>
2016-11-20 09:25:12 +0000

    first functional endpoint

--- api/app.py
+++ api/app.py
@@ -6,10 +6,10 @@ from resources import advertisements, reactions
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(advertisements.AdvertisementList, '/advertisements')
-api.add_resource(advertisements.AdvertisementRes, '/advertisements/<ad_id>')
-api.add_resource(reactions.AdReactionList, '/advertisements/<ad_id>/reactions')
-api.add_resource(reactions.AdReaction, '/advertisements/<ad_id>/reactions/<reaction_id>')
+# api.add_resource(advertisements.AdvertisementList, '/advertisements')
+# api.add_resource(advertisements.AdvertisementRes, '/advertisements/<ad_id>')
+api.add_resource(reactions.AdReactionList, '/ads')
+api.add_resource(reactions.AdReaction, '/ads/<ad_id>')
 
 if __name__ == '__main__':
   app.run(debug=True, use_debugger=True)
--- api/resources/reactions.py
+++ api/resources/reactions.py
@@ -1,18 +1,25 @@
 import json
+import os
 
 from flask_restful import Resource, Api
 
+data_dir = 'data/'
+
 class AdReaction(Resource):
-  def get(self, ad_id, reaction_id):
+  def get(self, ad_id):
     res = ''
-    with open('out.json', 'r') as f:
+    filepath = data_dir + ad_id
+    with open(filepath, 'r') as f:
       res = json.loads(f.read())
-  
-    return {
-      'id': 1,
-      'processing_result': res
-    }
+
+    return res
 
 class AdReactionList(Resource):
+
+  def files_in_data_dir(self):
+    for fn in os.listdir(data_dir):
+        if os.path.isfile(data_dir + fn) and fn.endswith('.json'):
+            yield fn
+
   def get(self):
-    return {'hello': 'world'}
+    return {'ads': list(self.files_in_data_dir())}
