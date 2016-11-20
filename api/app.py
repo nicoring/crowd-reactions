18f816fd7aa37361f255aa12e1f8f414b1a4f26b
Nico Ring <ringnico@web.de>
2016-11-20 09:31:59 +0000

    add cors

--- api/app.py
+++ api/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from resources import advertisements, reactions
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app)
 
 # api.add_resource(advertisements.AdvertisementList, '/advertisements')
